c03b16dad7bc2eaffce0f86a58cf97a23ff21b8c
Bellatrix Lugosi <cvar1984@cvar1984.net>
2026-09-24 16:35:48 +0700

    Replace PHP version info with phpinfo() output

--- www/index.php
+++ www/index.php
@@ -1,5 +1,2 @@
 <?php
-echo "PHP version: " . phpversion() . "\n";
-echo "disable_functions: " . ini_get('disable_functions') . "\n";
-echo "system() callable: " . (function_exists('system') && !in_array('system', explode(',', ini_get('disable_functions'))) ? 'yes' : 'no') . "\n";
-?>
+phpinfo();
